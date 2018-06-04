3037dbe6588dd2bc5096f1ff6513e670981648c3
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 16:41:53 -0400

    dependencies
    command: "npm install express mongoose passport passport-jwt jsonwebtoken body-parser bcryptjs validator"

--- main.js
+++ main.js
@@ -2,4 +2,18 @@
     Bryant Vail
     2-1_serverSetup
         
+    dependencies:
+        express.js
+        mongoose
+            data modeling
+        passport
+            auth
+        json web tokens
+            passport-jwt
+        jsonwebtoken
+        body-parser
+        bcryptjs
+        validator
+
+        command: "npm install express mongoose passport passport-jwt jsonwebtoken body-parser bcryptjs validator"
 */
