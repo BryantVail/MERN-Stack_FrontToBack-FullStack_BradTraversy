826c83155743f19e8f782469c35b5be648b6ec54
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 15:37:48 -0400

    github address

--- main.js
+++ main.js
@@ -2,7 +2,9 @@
     Bryant Vail
     1-3_before we begin
 
-    
+    Full project is on github
+        https://github.com/bradTraversy/devConnector
+    last video in each section will include the code for that section.
 
 
 */
