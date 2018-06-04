a900f3c4dafa6698db6b47772758ea6805cef527
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 15:23:43 -0400

    init

--- main.js
+++ main.js
@@ -1,34 +1,8 @@
 /*
     Bryant Vail
-        profiles, experience & creds
-        know node.js & React
+    1-2_MainApp
 
-    tech used:
-        ES6 + JavaScript
-            arrow functions
-        Node.js/Express
-        MongoDB
-        React & 
-        Redux
-            application state
-        Twitter bootstrap 4
-        Heroku & Git for Deployment
 
-    coursePlan:[
-        node/express
-            Create backend api with 
-        passport/JWT
-            sessions
-        'Postman'
-            test with 
-        bootstrap 
-            theme, Explore
-        React
-            connect w/backend
-        Redux
-            State management
-        Heroku
-            Build & deploy to Heroku
 
-    ]
+    
 */
