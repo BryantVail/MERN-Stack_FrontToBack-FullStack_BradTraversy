7f99408fa6816a5895c4d03dfced50db792c58c7
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 16:55:15 -0400

    hello successful

--- server.js
+++ server.js
@@ -2,7 +2,14 @@
 //Bryant Vail
 //server.js
 
+const express = require("express");
+const app = express();
 
+app.get("/",(req,res)=> res.send("Hello"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=> console.log(`Server Running on port: ${port}`));
 
 
 
