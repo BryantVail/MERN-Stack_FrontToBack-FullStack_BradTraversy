c7a71d5972773d93c8240288ddabcf427bd1f1a2
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 15:10:30 -0400

    course content

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+    Bryant Vail
+        profiles, experience & creds
+        know node.js & React
+
+    tech used:
+        ES6 + JavaScript
+            arrow functions
+        Node.js/Express
+        MongoDB
+        React & 
+        Redux
+            application state
+        Twitter bootstrap 4
+        Heroku & Git for Deployment
+*/
