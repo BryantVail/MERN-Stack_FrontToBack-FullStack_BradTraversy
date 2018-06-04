8bfde3ecc5134d49ee1f99562f35fe36ead2f1ff
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 16:34:46 -0400

    init

--- main.js
+++ main.js
@@ -1,14 +1,5 @@
 /*
     Bryant Vail
-    1-4_vsCodeSetup
-        CTRL + ,
-            brings up user settings
-    node.js
-
-    extentions
-        Bracket Pair colorizer
-        live server
-        node js module intellisence 
-        ES7 React/Redux/GraphQL/React-Native
+    2-1_serverSetup
         
 */
