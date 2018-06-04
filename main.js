167874ef4b8e62da1cfbd3d587d154da13ee72a5
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 15:15:21 -0400

    end vid1

--- main.js
+++ main.js
@@ -13,4 +13,22 @@
             application state
         Twitter bootstrap 4
         Heroku & Git for Deployment
+
+    coursePlan:[
+        node/express
+            Create backend api with 
+        passport/JWT
+            sessions
+        'Postman'
+            test with 
+        bootstrap 
+            theme, Explore
+        React
+            connect w/backend
+        Redux
+            State management
+        Heroku
+            Build & deploy to Heroku
+
+    ]
 */
