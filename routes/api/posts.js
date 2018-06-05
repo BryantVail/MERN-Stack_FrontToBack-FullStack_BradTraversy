e22f24c9f4a7f3a4ca6afec9c204a9d760346b78
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 20:35:36 -0400

    route descriptor
    //@route    GET api/posts/test
    //@desc     Tests post route
    //@access   public

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,10 @@
 const express = require("express");
 const router = express.Router();
 
-//http://localHost:5000/api/posts/test
+
+//@route    GET api/posts/test
+//@desc     Tests post route
+//@access   public
 router.get("/test", (req,res)=> res.json({msg:"Posts Works"}));
 //{"msg":"Profile Works"}
 
