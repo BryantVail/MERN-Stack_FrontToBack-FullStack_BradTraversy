aa7016a0949a3d4d4379d7aa138cf729a5156429
Bryant Vail <Bryanttv@outlook.com>
2018-06-04 22:19:42 -0400

    .gitignore

--- ()readMeDocs/projectCreds-Notes.js
+++ ()readMeDocs/projectCreds-Notes.js
@@ -21,6 +21,12 @@ let ProjectCreds = {
                     "command":"npm install -D nodemon",
                     "description":"watch node app & update for us instead of having to restart the app every    time.",
                     "response":"[nodemon] 1.17.5\n[nodemon] to restart at any time, enter `rs`\n[nodemon] watching: *.*\n[nodemon] starting `node server.js` \nServer Running on port: 5000"
+                },
+                {
+                    "name":"git init",
+                    "command":"git init",
+                    "description":"initializes git repository",
+                    "response":"Reinitialized existing Git repository in C:/Users/Dell/Documents/GitHub/MERN-Stack_FrontToBack-FullStack_BradTraversy/.git/"
                 }
     ],
     "routing":[
