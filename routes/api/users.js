effd47b50683a0b92b47b87fbda8555a39e91f62
Bryant Vail <Bryanttv@outlook.com>
2018-06-05 15:39:57 -0400

    gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -31,7 +31,19 @@ router.get("/test", (req, res)=> res.json({msg:"Users Works"}));
 //@desc     Register User
 //@access   Public
 router.post("/register",(req,res)=> {
-    User.findOne({email:req.body.email});
+    User.findOne({email:req.body.email})
+        .then(user => {
+            if(user){
+                return res.status(400).json({email: "email already exists"})
+            }else{
+                const newUser = new User({
+                    name:req.body.name, 
+                    email:req.body.email,
+                    avatar: avatar,
+                    password:req.body.password
+                });
+            }
+                
 });
 
 
